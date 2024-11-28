b08f4f562076ee9adfb73833e01b78027f0468fd
Error2H <154411778+Error2H@users.noreply.github.com>
2024-11-28 11:15:41 +0700

    Add files via upload

---
+++ 10354/Lab2/Lab3_8.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; 
+public class Lab3_8 { 
+    public static void main(String[] args) { 
+        Scanner scan = new Scanner(System.in); 
+        float hours, rate, salary; 
+        
+        System.out.print("Enter hours work : "); 
+        hours = scan.nextFloat(); 
+        System.out.print("Enter hourly rate : "); 
+        rate = scan.nextFloat(); 
+        //if ? true : flase
+        salary = (hours <= 40) ? (hours*rate) : (rate * (1.5f * hours - 20)); 
+        System.out.println("Employee get salary : " + salary); 
+        scan.close();
+    } 
+}
